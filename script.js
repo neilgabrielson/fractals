3c0729aa65afd4dc1c8d8f96e8632609407418b1
neilgabrielson <nagabrielson@gmail.com>
2025-05-23 18:09:06 -0400

    add julia sets

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 // script.js
 
 const resolution = 400;
+const iterations = 500;
+
 const mandelbrotCanvas = document.getElementById('mandelbrot');
+const juliaCanvas = document.getElementById('julia');
 
 mandelbrotCanvas.width = resolution;
 mandelbrotCanvas.height = resolution;
-
-const ctx = mandelbrotCanvas.getContext('2d');
-const imageData = ctx.createImageData(mandelbrotCanvas.width, mandelbrotCanvas.height);
-const pixels = imageData.data;
+juliaCanvas.width = resolution;
+juliaCanvas.height = resolution;
 
 const fc = (z, c) => [z[0]**2-z[1]**2+c[0], 2*z[0]*z[1]+c[1]];
 const norm_sq = z => z[0]**2 + z[1]**2;
 
-var L = [[-2,2],[-2,2]]
+var mandelbrotDomain = [[-2,2],[-2,2]]
 
-function escape_time(c) {
-    let z = [0,0];
-    for (let n = 1; n < 100; n++) {
+function escape_time(z, c) {
+    for (let n = 1; n < iterations; n++) {
         z = fc(z,c);
         if (norm_sq(z) >= 4) return n;
     }
@@ -37,18 +37,52 @@ function colormap(x) {
     return [x,x,255-x]
 }
 
-function plot_mandelbrot(domain, pointer) {
-    for (let r = 0; r < resolution; r++) {
-        for (let i = 0; i < resolution; i++) {
-            if (r === pointer[0] && i === pointer[1]) setPixel(r, i, 255, 0, 0)
-            else {
-                const iterations = escape_time([
-                    r / resolution * (domain[0][1]-domain[0][0]) + domain[0][0],
-                    i / resolution * (domain[1][1]-domain[1][0]) + domain[1][0]
-                ]);
-                const color = iterations === 0 ? [0,0,0] : colormap(iterations/100);
-                setPixel(r, i, color[0], color[1], color[2]);
-            }
+function plot_mandelbrot(canvas, domain) {
+    const ctx = canvas.getContext("2d");
+    const imageData = ctx.createImageData(canvas.width, canvas.height);
+    const pixels = imageData.data;
+    for (let x = 0; x < resolution; x++) {
+        for (let y = 0; y < resolution; y++) {
+            const it = escape_time(
+                [0,0],
+                [
+                    x / resolution * (domain[0][1]-domain[0][0]) + domain[0][0],
+                    y / resolution * (domain[1][1]-domain[1][0]) + domain[1][0]
+                ]
+            );
+            const color = it === 0 ? [0,0,0] : colormap(it/100);
+            const index = (y * canvas.width + x) * 4;
+            pixels[index] = color[0];
+            pixels[index + 1] = color[1];
+            pixels[index + 2] = color[2];
+            pixels[index + 3] = 255;
+        }
+    }
+    ctx.putImageData(imageData, 0, 0);
+}
+
+function plot_julia(canvas, domain, z) {
+    const ctx = canvas.getContext("2d");
+    const imageData = ctx.createImageData(canvas.width, canvas.height);
+    const pixels = imageData.data;
+    for (let x = 0; x < resolution; x++) {
+        for (let y = 0; y < resolution; y++) {
+            const it = escape_time(
+                [
+                    x / resolution * (domain[0][1]-domain[0][0]) + domain[0][0],
+                    y / resolution * (domain[1][1]-domain[1][0]) + domain[1][0]
+                ],
+                [
+                    z[0],
+                    z[1]
+                ]
+            );
+            const color = it === 0 ? [0,0,0] : colormap(it/100);
+            const index = (y * canvas.width + x) * 4;
+            pixels[index] = color[0];
+            pixels[index + 1] = color[1];
+            pixels[index + 2] = color[2];
+            pixels[index + 3] = 255;
         }
     }
     ctx.putImageData(imageData, 0, 0);
@@ -66,14 +100,16 @@ const scale = (domain, factor, p) =>
         ]
     ];
 
-plot_mandelbrot([[-2,2],[-2,2]], [100,100])
+plot_mandelbrot(mandelbrotCanvas, mandelbrotDomain)
+plot_julia(juliaCanvas, mandelbrotDomain, [0,0.5])
 
 mandelbrotCanvas.addEventListener('click', (event) => {
     const rect = mandelbrotCanvas.getBoundingClientRect();
     const pixelX = event.clientX - rect.left;
     const pixelY = event.clientY - rect.top;
-    const pr = pixelX / resolution * (L[0][1]-L[0][0]) + L[0][0];
-    const pi = pixelY / resolution * (L[1][1]-L[1][0]) + L[1][0];
-    L = scale(L,0.5,[pr,pi])
-    plot_mandelbrot(L, [Math.round(pixelX),Math.round(pixelY)])
+    const pr = pixelX / resolution * (mandelbrotDomain[0][1]-mandelbrotDomain[0][0]) + mandelbrotDomain[0][0];
+    const pi = pixelY / resolution * (mandelbrotDomain[1][1]-mandelbrotDomain[1][0]) + mandelbrotDomain[1][0];
+    // mandelbrotDomain = scale(mandelbrotDomain,0.5,[pr,pi])
+    // plot_mandelbrot(mandelbrotCanvas, mandelbrotDomain)
+    plot_julia(juliaCanvas, mandelbrotDomain, [pr,pi])
 });
